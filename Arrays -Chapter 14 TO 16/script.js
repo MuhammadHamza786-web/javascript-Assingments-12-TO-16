ebe3831580df15ece424e8a5068fae50dfc22b2b
MuhammadHamza786-web <hamzasiraj.470@gmail.com>
2026-01-08 14:24:07 +0500

    Update script.js
    Add code

--- Arrays -Chapter 14 TO 16/script.js
+++ Arrays -Chapter 14 TO 16/script.js
@@ -81,3 +81,118 @@ console.log(cities);
 let arr=["This ", "is" , "my" , "cat" ];
  let result=arr.join("");
  console.log(result);
+
+
+  //  9. Initialize an array with color names. Display the array
+// elements in your browser.
+// a. Ask the user what color he/she wants to add to the
+// beginning & add that color to the beginning of the array.
+// Display the updated array in your browser.
+// b. Ask the user what color he/she wants to add to the end
+// & add that color to the end of the array. Display the
+// updated array in your browser.
+// c. Add two more color to the beginning of the array.
+// Display the updated array in your browser.
+// d. Delete the first color in the array. Display the updated
+// array in your browser.
+// e. Delete the last color in the array. Display the updated
+// array in your browser.
+// f. Ask the user at which index he/she wants to add a color 
+// & color name. Then add the color to desired
+// position/index. . Display the updated array in your
+// browser.
+// g. Ask the user at which index he/she wants to delete
+// color(s) & how many colors he/she wants to delete. Then
+// remove the same number of color(s) from user-defined
+// position/index. . Display the updated array in your
+// browser.
+
+
+//  9. Initialize an array with color names. Display the array
+// elements in your browser.
+
+let colorName=["Red","Green","Blue"];
+
+alert(colorName);
+
+// // a. Ask the user what color he/she wants to add to the
+// // beginning & add that color to the beginning of the array.
+// // Display the updated array in your browser.
+
+let addStart=prompt("Enter the color name to add array at the start:");
+
+colorName.unshift(addStart);
+
+alert(colorName);
+
+// // b. Ask the user what color he/she wants to add to the end
+// // & add that color to the end of the array. Display the
+
+let addEnd=prompt("Enter the color name to add array at the End");
+
+colorName.push(addEnd);
+
+alert(colorName);
+
+// // c. Add two more color to the beginning of the array.
+// // Display the updated array in your browser.
+
+
+let add_Two_Value_Start=prompt("Enter the two color name to add array at the start:");
+
+colorName.unshift(add_Two_Value_Start);
+
+alert(colorName);
+
+// // d. Delete the first color in the array. Display the updated
+// // array in your browser.
+
+let delete_First_Color=prompt("Enter the color name to delete array at the Start");
+alert("Delete first color to the array start");
+colorName.shift();
+alert(colorName);
+
+// // f. Ask the user at which index he/she wants to add a color 
+// // & color name. Then add the color to desired
+// // position/index. . Display the updated array in your
+// // browser.
+
+let addUserColor=prompt("Enter the color name:");
+let indexNum=prompt("which index add the color:");
+
+indexNum=Number(indexNum);
+ 
+colorName.splice(indexNum,0,addUserColor);
+
+alert(colorName);
+
+
+// // g. Ask the user at which index he/she wants to delete
+// // color(s) & how many colors he/she wants to delete. Then
+// // remove the same number of color(s) from user-defined
+// // position/index. . Display the updated array in your
+// // browser.
+
+let indexNumber=prompt("which index delete the color:");
+
+indexNumber=Number(indexNumber);
+
+colorName.splice(indexNumber,indexNumber);
+
+alert(colorName);   
+
+// 13. Create a new array. Store values one by one in such a way
+// that you can access the values in the order in which they
+// were stored. (FIFO-First In First Out)
+
+
+let  computerAssosaries=["Keyboard","Mouse","Printer","Monitor"];
+
+alert(computerAssosaries+"\n"+ "out:"+"\n"+computerAssosaries[0]+"\n"+"out:"+"\n"+computerAssosaries[1]+"\n"+"out:"+"\n"+computerAssosaries[2]+"\n"+"out:"+"\n"+computerAssosaries[3]);
+
+// 14. Create a new array. Store values one by one in such a way
+// that you can access the values in reverse order. (Last InFirst Out)
+
+let  computersAssosaries=["Keyboard","Mouse","Printer","Monitor"];
+
+alert(computerAssosaries+"\n"+ "out:"+"\n"+computerAssosaries[3]+"\n"+"out:"+"\n"+computerAssosaries[2]+"\n"+"out:"+"\n"+computerAssosaries[1]+"\n"+"out:"+"\n"+computerAssosaries[0]);
